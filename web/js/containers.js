8e322962478cb3c3e936a9196a72228f8f5096a5
Vitor <vi.lupinetti@hotmail.com.br>
2021-11-19 20:32:57 -0300

    funcao que nao funciona :(

--- web/js/containers.js
+++ web/js/containers.js
@@ -41,6 +41,16 @@ window.onload = () => {
         
                                 </div>
                                 <div class="d-flex">
+                                    <span class="btn card_btn" id="view${container.id}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                                            class="bi bi-eye" viewBox="0 0 16 16">
+                                            <path
+                                                d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                                            <path
+                                                d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                                        </svg>
+                                        <p>Visualizar</p>
+                                    </span>
                                     <span class="btn card_btn" onclick="deleteContainer('${container.id}')">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
                                             class="bi bi-trash" viewBox="0 0 16 16">
@@ -58,6 +68,14 @@ window.onload = () => {
                     </li>
                     `
                 cardsContainer.innerHTML += card;
+                let view = document.getElementById(`view${container.id}`);
+                if (container.status !== 'running') {
+                    view.hidden = true;
+                }
+                else {
+                    view.addEventListener('click', () => viewContainer(Object.values(container.ports[0])[0]))
+                }
+
             }
         })
         .catch(function (error) {
@@ -154,4 +172,9 @@ function stopContainer(id) {
             document.location.reload(true);
         });
 
+}
+
+function viewContainer(port) {
+    console.log(port)
+    // window.open(`${baseUrl2}:${port}`, "_blank")
 }
--- web/js/globalUrl.js
+++ web/js/globalUrl.js
@@ -1,1 +1,2 @@
 var baseUrl = 'http://192.168.0.24:5000'
+var baseUrl2 = 'http://192.168.0.24'
