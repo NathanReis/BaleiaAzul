e39b7260193155bbbd2fbdf7c06a496ad5d9ab48
Vitor <vi.lupinetti@hotmail.com.br>
2021-11-18 23:52:07 -0300

    icone loading adicionado em requisicoes demmoradas e opcao de deletar todos

--- web/js/images.js
+++ web/js/images.js
@@ -20,16 +20,7 @@ window.onload = () => {
                                         <h2 class="card_title">${image.tags[0].split(':')[0]}</h2>
                                         <p class="card_text">${image.tags[0].split(':')[1]}</p>
                                         <div class="d-flex">
-                                            <span class="btn card_btn">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                                                    class="bi bi-eye" viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                                                    <path
-                                                        d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                                                </svg>
-                                                <p>Visualizar</p>
-                                            </span>
+                                           
                                             <span onclick="deleteImage('${image.id}')" class="btn card_btn">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
                                                     class="bi bi-trash" viewBox="0 0 16 16">
@@ -65,6 +56,18 @@ function deleteImage(id) {
         })
 }
 
+function deleteAllImages() {
+    const url = baseUrl + '/images';
+    fetch(url, {
+        method: 'DELETE',
+    })
+        .then(res => res.text()) // or res.json()
+        .then(res => {
+            console.log(res)
+            document.location.reload(true);
+        })
+}
+
 function addImage() {
     const url = baseUrl + '/images';
 
@@ -79,9 +82,16 @@ function addImage() {
         headers: { 'Content-Type': 'application/json' }
     }
 
+    let spinner = document.getElementById('loading');
+    let btn = document.getElementById('btn_add');
+    btn.hidden = true;
+    spinner.hidden = false;
+
     fetch(url, fetchData)
         .then(function (res) {
             console.log(res);
+            btn.hidden = false;
+            spinner.hidden = true;
             document.location.reload(true);
         });
 
